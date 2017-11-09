d04b2403d478fa2632cd1c41924424e31addc8d2
Ryan Hauptman <ryanhauptman@RyanComputer.local>
2017-11-09 07:58:34 -0500

    conflicting changes lets see what happens

--- Notes/Unit 2/project-2-beat-mix/presetHandler.js
+++ Notes/Unit 2/project-2-beat-mix/presetHandler.js
@@ -2,12 +2,19 @@
 const presets = require('./presets');
 
 // Complete this function:
-const presetHandler = (requestType,arrayIndex,presets) => {
+const presetHandler = (requestType,arrayIndex,newArray) => {
   if(isValidRequestType(requestType)===false) {//checks if the request is valid
     return [400,null]; //returns 400 error when request is not valid
   } else if (isValidIndex(index)===false){ //checks if index is valid
     return [404,null]; //returns 404 error when index is not valid
   } else {
+    if (requestType === "GET") { //for get requests...
+      return [200, presets.indexOf(arrayIndex)];//return the value at that position of the array
+    } else { //request type is PUT
+      presets[arrayIndex] = newArray;
+      return [200,newArray];
+    }
+
     return [200,presets];
   }
 
@@ -25,7 +32,7 @@ const isValidRequestType = (requestType) => {
 
 //checks to see if
 const isValidIndex = (arrayIndex) => {
-  if (index >= 0) {
+  if (index >= 0 && index < presets.length) {
     return true;
   } else {
     return false;
